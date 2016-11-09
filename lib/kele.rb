0823da0db9b44a1a016e99224cd91452dd76626c
Drew <rittera13@gmail.com>
2016-11-09 14:43:30 -0600

    API checkpoint 3, add a method to pull user data and parse it into a Ruby hash.

--- lib/kele.rb
+++ lib/kele.rb
@@ -1,4 +1,5 @@
 require 'httparty'
+require 'json'
 
 class Kele
   include HTTParty
@@ -6,6 +7,11 @@ class Kele
   def initialize(email, password)
     reply = self.class.post(api_url("sessions"), body: { email: email, password: password } )
     @auth_token = reply['auth_token']
+  end
+
+  def get_me
+    reply = self.class.get(api_url("users/me"), headers: { "authorization" => @auth_token })
+    readable = JSON.parse(reply.body)
   end
 
   private
